b9d339e27425fc7a2b1bf76d3d43a24821a543ad
VitorinoAlves <vitorino.17alexandre@gmail.com>
2025-04-28 05:45:37 -0300

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int primeiraNota, segundaNota, terceiraNota, quartaNota, media;
+
+        System.out.println("Calculadora de médias");
+        System.out.printf("Informe a primeira nota: ");
+        primeiraNota = scanner.nextInt();
+
+        System.out.printf("Informe a segunda nota: ");
+        segundaNota = scanner.nextInt();
+
+        System.out.printf("Informe a terceira nota: ");
+        terceiraNota = scanner.nextInt();
+
+        System.out.printf("Informe a quarta nota: ");
+        quartaNota = scanner.nextInt();
+
+        media = (primeiraNota + segundaNota + terceiraNota + quartaNota)/4;
+
+        System.out.println("Sua média é: " + media);
+    }
+}
